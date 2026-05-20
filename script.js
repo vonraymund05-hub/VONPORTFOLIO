52e76f8d7489f76c5c81c0da6d7fbfaf974c2a5a
vonraymund05-hub <vonraymund05@gmail.com>
2026-05-20 21:06:55 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 function toggleDarkMode() {
     document.body.classList.toggle("dark-mode");
+
+    if(document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+}
+
+window.onload = () => {
+    if(localStorage.getItem("theme") === "dark") {
+        document.body.classList.add("dark-mode");
+    }
+};
+
+const darkBtn = document.querySelector(".dark-btn");
+
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+
+    if(document.body.classList.contains("dark-mode")) {
+        darkBtn.innerHTML = "☀️";
+    } else {
+        darkBtn.innerHTML = "🌙";
+    }
 }
